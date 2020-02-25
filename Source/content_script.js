ea5e74f28e8c681241e54e0283fdbd7b4af5e091
RansomTime <47819665+RansomTime@users.noreply.github.com>
2020-02-25 16:14:55 +0000

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bUnisex\b/g, "Emo");
+	v = v.replace(/\bunisexd\b/g, "emo");
 	
 	textNode.nodeValue = v;
 }
